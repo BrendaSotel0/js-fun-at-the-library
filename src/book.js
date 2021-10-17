f7b8f8c46c5d6aa178139afd7456ff9c211aba30
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2021-10-17 17:12:20 -0600

    Initial commit

--- src/book.js
+++ src/book.js
@@ -1,25 +1,49 @@
 function createTitle(bookTitle,) {
   return `The ${bookTitle}`
-
 }
 
-function buildMainCharacter(name, age, pronouns) {
+function buildMainCharacter(a, b, c) {
   var character = {
-    name: name,
-    age: age,
-    pronouns: pronouns,
+    name: a,
+    age: b,
+    pronouns: c,
   }
   return character
 }
-function saveReview() {
 
+function saveReview(addString, reviews) {
+  if(reviews.includes(addString)) {
+    return reviews
+  } else {
+    reviews.push(addString)
+  }
+  return reviews
 }
 
+function calculatePageCount(newTitle) {
+  return 20 * newTitle.length;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var bookObject = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(bookTitle)
+  }
+  return bookObject
+}
+
+function editBook(newBook) {
+  newBook.pageCount = newBook.pageCount * 0.75
+}
+
+
 module.exports = {
-  createTitle,
+   createTitle,
    buildMainCharacter,
    saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+   calculatePageCount,
+   writeBook,
+   editBook
 }
