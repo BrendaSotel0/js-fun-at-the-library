378649454e43f22ed9abc754423cc3f19d1abba7
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2021-10-17 17:08:37 -0600

    Initial commit

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,34 @@
+function shelfBook(bookName, shelfType) {
+  if(shelfType.length < 3) {
+    shelfType.unshift(bookName)
+  }
+}
+
+function unshelfBook(bookName, shelfType) {
+    for (var i = 0; i < shelfType.length; i++) {
+      if(shelfType[i].title === bookName) {
+        shelfType.splice(i, 1)
+      }
+    }
+}
+
+function listTitles(shelfType) {
+  var titleShelf = [shelfType[0].title, shelfType[1].title, shelfType[2].title];
+  return titleShelf.join(', ');
+}
+
+function searchShelf(shelfType, bookTitle) {
+  for (var i = 0; i < shelfType.length; i++) {
+    if (shelfType[i].title === bookTitle) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+   shelfBook,
+   unshelfBook,
+   listTitles,
+   searchShelf
 };
