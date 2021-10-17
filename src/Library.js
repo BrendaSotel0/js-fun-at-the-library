c71ec9eb35d664e9365e9d7751e518dbb4b2f975
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2021-10-17 16:44:41 -0600

    Initial commit

--- src/Library.js
+++ src/Library.js
@@ -10,11 +10,21 @@ function createLibrary(a) {
   return library
 }
 function addBook(theLibrary, theBook) {
-  theLibrary.shelves.fantasy.push(theBook)
+  theLibrary.shelves[theBook.genre].push(theBook)
 }
+function checkoutBook(theLibrary, theBook, theShelf) {
+  for(var i = 0; i < theLibrary.shelves[theShelf].length; i++) {
+    if(theLibrary.shelves[theShelf][i].title === theBook) {
+      theLibrary.shelves[theShelf].splice(i, 1);
+      return `You have now checked out ${theBook} from the ${theLibrary.name}`
+    }
+  }
+  return `Sorry, there are currently no copies of ${theBook} available at the ${theLibrary.name}`
+}
+
 
 module.exports = {
    createLibrary,
    addBook,
-  // checkoutBook
+   checkoutBook
 };
